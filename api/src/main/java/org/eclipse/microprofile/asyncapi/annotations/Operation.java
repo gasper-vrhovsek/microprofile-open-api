cb999de3fdeecf76aeb2cd6037dda9bc8ddf5a0b
GasperV <gasper.vrhovsek@global.id>
2021-05-04 14:12:53 +0200

    Add operation annotation properties

--- api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/Operation.java
+++ api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/Operation.java
@@ -1,5 +1,59 @@
 package org.eclipse.microprofile.asyncapi.annotations;
 
+import org.eclipse.microprofile.asyncapi.annotations.bindings.OperationBindings;
+import org.eclipse.microprofile.asyncapi.annotations.components.Message;
+import org.eclipse.microprofile.asyncapi.annotations.components.OperationTrait;
+import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 public @interface Operation {
-    // TODO this annotation
+    // TODO docs
+
+    /**
+     *
+     * @return
+     */
+    String operationId() default "";
+
+    /**
+     *
+     * @return
+     */
+    String summary() default "";
+
+    /**
+     *
+     * @return
+     */
+    String description() default "";
+
+    /**
+     *
+     * @return
+     */
+    Tag[] tags() default {};
+
+    /**
+     *
+     * @return
+     */
+    ExternalDocumentation externalDocs() default @ExternalDocumentation();
+
+    /**
+     *
+     * @return
+     */
+    OperationBindings bindings() default @OperationBindings();
+
+    /**
+     *
+     * @return
+     */
+    OperationTrait[] traits() default {};
+
+    /**
+     *
+     * @return
+     */
+    Message message();
 }
