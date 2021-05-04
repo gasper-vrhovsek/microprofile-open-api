16c2e8944b5023bf9ea59e45407366a5a3c72491
GasperV <gasper.vrhovsek@global.id>
2021-05-04 14:11:16 +0200

    Add correlationId annotation properties

--- api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/components/CorrelationId.java
+++ api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/components/CorrelationId.java
@@ -1,5 +1,17 @@
 package org.eclipse.microprofile.asyncapi.annotations.components;
 
 public @interface CorrelationId {
-    // TODO
+    // TODO docs
+
+    /**
+     *
+     * @return
+     */
+    String description() default "";
+
+    /**
+     *
+     * @return
+     */
+    String location() default "";
 }
