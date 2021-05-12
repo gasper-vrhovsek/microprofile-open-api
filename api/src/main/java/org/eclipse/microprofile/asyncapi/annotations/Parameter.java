13e368dd0e11ec320178bbc32fcec0df99e18525
GasperV <gasper.vrhovsek@global.id>
2021-05-12 08:30:22 +0200

    Parameter annotation docs

--- api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/Parameter.java
+++ api/src/main/java/org/eclipse/microprofile/asyncapi/annotations/Parameter.java
@@ -2,30 +2,44 @@ package org.eclipse.microprofile.asyncapi.annotations;
 
 import org.eclipse.microprofile.asyncapi.annotations.media.Schema;
 
+/**
+ * Describes operations available on a single channel
+ * */
 public @interface Parameter {
-    // TODO docs
-
     /**
+     *  Optional description of this channel item. <a href="http://spec.commonmark.org/">CommonMark</>
+     *  syntax can be used for rich text representation.
      *
-     * @return
-     */
+     * @return channel description
+     * */
     String description() default "";
 
     /**
+     * Definition of the parameter.
      *
-     * @return
-     */
+     * @return channels schema
+     * */
     Schema schema() default @Schema();
 
     /**
+     * <a href="https://github.com/asyncapi/spec/blob/master/spec/asyncapi.md#runtimeExpression">Runtime expression</a>
+     * that specifies the location of the parameter value. Even when a definition for the target field exists, it MUST NOT be used to validate
+     * this parameter but, instead, the schema property MUST be used.
      *
-     * @return
-     */
+     * @return location property
+     * */
     String location() default "";
 
     /**
+     * The name of the parameter.
+     * <p>
+     * The name is REQUIRED when the parameter is defined within
+     * {@link org.eclipse.microprofile.asyncapi.annotations.channels.ChannelItem} or
+     * {@link org.eclipse.microprofile.asyncapi.annotations.components.Components}.
+     * The name will be used as the key to add this schema to the 'parameters' map for reuse.
+     * </p>
      *
-     * @return
-     */
+     * @return the name of the schema
+     **/
     String name() default "";
 }
